094000e6e4b5738e2917f783cb5e462fbe9c00e7
Arno Töll <arno@debian.org>
2013-07-21 11:55:38 +0200

    Imported Debian patch 3.2.5-1

---
+++ debian/change_config.pl
@@ -0,0 +1,31 @@
+#! /usr/bin/perl
+
+use strict;
+
+our @config_lines;
+our %defaults = (
+    "proxy.config.alarm_email" => "trafficserver",
+    "proxy.config.admin.user_id" => "trafficserver",
+    "proxy.config.log.max_space_mb_for_logs" => 2000,
+);
+
+die("Usage: $0 <file name>") unless $#ARGV == 0;
+
+open(F, "+<", $ARGV[0]) || die("Cannot open $ARGV[0]: $!");
+while(my $line = <F>)
+{
+        if ($line =~ /^CONFIG/)
+        {
+                foreach my $pattern (keys %defaults)
+                {
+                    $line =~ s/(?<=CONFIG $pattern)(\s+[A-Z]+)\s+.+$/$1 $defaults{$pattern}/;
+                }
+        }
+        push(@config_lines, $line);
+}
+seek(F, 0, 0);
+foreach my $line (@config_lines)
+{
+        print F $line;
+}
+close(F);
